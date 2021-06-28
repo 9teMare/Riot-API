fe7e86c1d8fda1e4e66f00e037780c9ea367ad61
9teMare <wangzihao139@126.com>
2021-06-28 17:36:22 +0800

    changed not working ES syntax

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import jsonServer from 'json-server';
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('apiKey.json');
 const middlewares = jsonServer.defaults();
